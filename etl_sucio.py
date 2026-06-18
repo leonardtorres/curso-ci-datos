a7b4a5939e7bcf5f2e97c018e9d7b8f23aab03ea
Leonard Torres <leonardtorresrodriguez@gmail.com>
2026-06-18 17:00:02 -0500

    Subiendo v2.3 de proyecto

--- etl_sucio.py
+++ etl_sucio.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def mi_funcion_extracccion():
     #X = "Variable que no uso"
-    data={"col1":[1,2],"col2":[3,4]}
+    data={"col1": [1, 2], "col2": [3, 4]}
     df=pd.DataFrame(data)
     print("Procesando...")
     return df
